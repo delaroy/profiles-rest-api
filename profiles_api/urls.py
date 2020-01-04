209b9d2a946ea6b7dd077cacc804ee4f751b5fad
delaroystudios <RepubliC7891>
2020-01-04 10:20:35 +0100

    Added Post, Put, Patch and delete methods

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,6 +1,6 @@
-from djanggo.urls import path
+from django.urls import path
 from profiles_api import views
 
 urlpatterns = [
-    path('hello-view/', views.HelloApiView.asview()),
+    path('hello-view/', views.HelloApiView.as_view())
 ]
